a1fde21c6220281fbf1e6ea9cd4950a65d9344ae
Parker Knight <plb.knight@gmail.com>
2020-08-06 15:59:26 -0400

    Add parameter documentation to estimation and simulation steps

--- R/estimate.R
+++ R/estimate.R
@@ -1,5 +1,23 @@
 #' Estimate scaffold parameters from a SingleCellExperiment.
 #'
+#' @param sce A SingleCellExperiment object used to estimate the scaffold parameters.
+#' @param numCells The number of cells to be used in the simulation. If left NULL, the number of cells in the \code{sce} object is used.
+#' @param numGenes The number of genes to be used in the simulation. If left NULL, the number of genes in the \code{sce} object is used.
+#' @param geneMeans The mean expression level of each gene. If left NULL, the means are estimated using the \code{sce} object.
+#' @param geneTheta XXTODOXX
+#' @param genes A vector of names for each gene. If left NULL, the gene names from the \code{sce} object are used.
+#' @param captureEfficiency XXTODOXX
+#' @param typeOfAmp The amplification method used in the simulation, defaults to "PCR".
+#' @param numFirstAmpCycles The number of cycles in the first amplification stage of the simulation.
+#' @param numSecondAmpCycles The number of cycles in the second amplification stage of the simulation.
+#' @param firstAmpEfficiency A value between 0 and 1 indicating the efficiency of the first simulated amplification cycle.
+#' @param secondAmpEfficiency A value between 0 and 1 indicating the efficiency of the second simulated amplification cycle.
+#' @param tagEfficiency XXTODOXX
+#' @param degree Numeric, determines the amount of perturbation applied to the simulated initial gene counts.
+#' @param percentRange A value between 0 and 1 indicating the number of samples that undergo exct diluation in the equalization step of the simulation. A negative value indicates that all samples are diluated by the same factor, resulting in no equalization performed.
+#' @param protocol The protocol to model in the simulation.
+#' @param totalSD The total sequencing depth of the simulated data. If left NULL, this is taken from the \code{sce} object.
+#'
 #' @importFrom stats rnorm runif var
 #' @importFrom methods new
 #' @export
