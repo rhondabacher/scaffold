13571cadc70d0f206645bd1c7d3ddf4ab80c0b42
Parker Knight <plb.knight@gmail.com>
2020-08-07 10:49:30 -0400

    Improve documentation for estimation of scaffold params, fix typos

--- R/estimate.R
+++ R/estimate.R
@@ -4,17 +4,17 @@
 #' @param numCells The number of cells to be used in the simulation. If left NULL, the number of cells in the \code{sce} object is used.
 #' @param numGenes The number of genes to be used in the simulation. If left NULL, the number of genes in the \code{sce} object is used.
 #' @param geneMeans The mean expression level of each gene. If left NULL, the means are estimated using the \code{sce} object.
-#' @param geneTheta XXTODOXX
+#' @param geneTheta The theta parameter used if the negative binomial distribution is chosen to generate the initial counts for each gene. Should be a numeric vector of length \code{numGenes}. If left NULL, this value is estimated from the \code{sce} object.
 #' @param genes A vector of names for each gene. If left NULL, the gene names from the \code{sce} object are used.
-#' @param captureEfficiency XXTODOXX
+#' @param captureEfficiency A value between 0 and 1 indicating the proportion of mRNA molecules successfully captured for the genes in each cell. If left NULL, this value is estimated using the \code{sce} object.
 #' @param typeOfAmp The amplification method used in the simulation, defaults to "PCR".
 #' @param numFirstAmpCycles The number of cycles in the first amplification stage of the simulation.
 #' @param numSecondAmpCycles The number of cycles in the second amplification stage of the simulation.
-#' @param firstAmpEfficiency A value between 0 and 1 indicating the efficiency of the first simulated amplification cycle.
+#' @param firstAmpEfficiency A value between 0 and 1 indicating the efficiency of the first simulated amplification cycle. If set to 1, then all molecules will double each cycle.
 #' @param secondAmpEfficiency A value between 0 and 1 indicating the efficiency of the second simulated amplification cycle.
-#' @param tagEfficiency XXTODOXX
+#' @param tagEfficiency A value between 0 and 1 indicating the tagmentation efficiency.
 #' @param degree Numeric, determines the amount of perturbation applied to the simulated initial gene counts.
-#' @param percentRange A value between 0 and 1 indicating the number of samples that undergo exct diluation in the equalization step of the simulation. A negative value indicates that all samples are diluated by the same factor, resulting in no equalization performed.
+#' @param percentRange A value between 0 and 1 indicating the number of samples that undergo exact dilution in the equalization step of the simulation. A negative value indicates that all samples are diluted by the same factor, resulting in no equalization performed.
 #' @param protocol The protocol to model in the simulation.
 #' @param totalSD The total sequencing depth of the simulated data. If left NULL, this is taken from the \code{sce} object.
 #'
--- R/simulate.R
+++ R/simulate.R
@@ -4,7 +4,7 @@
 #' @param originalSCE The SingleCellExperiment used to create the scaffoldParams object.
 #' @param model The probability distribution used to generate the initial gene counts. Defaults to 'p' for the Poisson distribution, but can also be set to 'nb' to use the Negative Binomial distribution.
 #' @param inputInitial A optional matrix of initial gene counts which should have the same dimension indicated in the \code{scaffoldParams} parameter. If left NULL, the initial counts will be generated according to the distribution indicated by the \code{model} parameter.
-#' @param SD The sequencing depth used to estimate capture efficiency; only used if the captureEfficient slot is not set in the \code{scaffoldParams} object.
+#' @param SD The sequencing depth used to estimate capture efficiency; only used if the captureEfficiency slot is not set in the \code{scaffoldParams} object.
 #'
 #' @importFrom SingleCellExperiment SingleCellExperiment
 #' @export
