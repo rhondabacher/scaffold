22068f17094a25d916bab83e76b7bb3defe4d8a6
Rhonda Bacher <rhondabacher@gmail.com>
2021-05-27 14:59:43 -0400

    update vignette

--- R/estimate.R
+++ R/estimate.R
@@ -28,7 +28,7 @@
 #' @importFrom methods new
 #' @importFrom Rfast Rnorm comb_n
 #' @export
-estimateScaffoldParams <- function(sce, sceUMI = FALSE, numCells = NULL, numGenes = NULL, geneMeans = NULL,
+estimateScaffoldParameters <- function(sce, sceUMI = FALSE, numCells = NULL, numGenes = NULL, geneMeans = NULL,
       totalTranscripts = NULL, genes = NULL, protocol = "C1", useUMI = FALSE, 
 			popHet = NULL,
 			geneEfficiency = NULL, 
