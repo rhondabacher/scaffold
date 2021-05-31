ca48eb8f8300fb8d9c9f3f8f2c18a91ba6c3e15a
rhondabacher <rhonda.bacher@gmail.com>
2021-05-30 23:13:48 -0400

    update vignette

--- R/initialCounts.R
+++ R/initialCounts.R
@@ -29,19 +29,31 @@ generateGeneCounts <- function(numCells, mu, popHet) {
 generateDynamicGeneCounts <- function(numCells, mu, dynamicParams) {
   
 
-  selectGenes <- sample(1:length(mu), ceiling(dynamicParams$propDynamic*length(mu)))
-  otherGenes <- setdiff(1:length(mu), selectGenes)
-  
+    selectGenes <- sample(names(mu), ceiling(dynamicParams$propGenes*length(mu)))
+    otherGenes <- setdiff(names(mu), selectGenes)
+
   R0 <- matrix(sapply(otherGenes, function(x) rpois(numCells, mu[x])), nrow=numCells)
 
+  if (is.null(dynamicParams$degree)) dynamicParams$degree <- 2
+
+  ngenes <- length(selectGenes)      
+  if (is.null(dynamicParams$knots)) {
+       allknots <- cbind(runif(ngenes, 0, .5), runif(ngenes, .5, 1)) # Two sites of major change
+  } else {allknots <- dynamicParams$knots}
+  if (is.null(dynamicParams$theta)) {
+      alltheta <- matrix(rnorm(5, 5, 5), ncol=5, nrow=ngenes) # Directional Changes (num knot+degree+1)
+  } else {alltheta <- dynamicParams$theta}
+  
+  rownames(allknots) <- selectGenes
+  rownames(alltheta) <- selectGenes
+  
   R1 <- sapply(selectGenes, function(x){
     genpts <- sort(runif(numCells))
-    knots <- c(runif(1,0, .5), runif(1,.5, 1)) # Two sites of major change
-    # Directional Changes (num knot+degree+1)
-    theta <- c(rnorm(1, 5, 5), rnorm(1, 5, 5), rnorm(1, 5, 5), rnorm(1, 5, 5), rnorm(1, 5, 5))
+    knots <- allknots[x,] 
+    theta <- alltheta[x,]
     theta <- sqrt(mu[x])*scale(theta, T, F) + mu[x]
     theta[theta<0] <- 0
-    basis <- bs(x = genpts, knots = knots, degree = 2,
+    basis <- bs(x = genpts, knots = knots, degree = dynamicParams$degree,
                 Boundary.knots = c(0,1), intercept = TRUE)
     resp <- basis %*% theta
     use_dt <- data.table(xvals = genpts, yvals = resp[,1])
